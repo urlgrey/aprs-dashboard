cae45ab3ce928dc88212d71678d0e98a73f8a039
Scott Kidder <kidder.scott@gmail.com>
2014-11-11 17:47:59 -0800

    Parse weather report

--- aprs_parser.go
+++ aprs_parser.go
@@ -9,18 +9,23 @@ import "errors"
 import "time"
 
 type AprsMessage struct {
-	Timestamp           int32   `json:"timestamp"`
-	SourceCallsign      string  `json:"src_callsign"`
-	DestinationCallsign string  `json:"dst_callsign"`
-	Status              string  `json:"status"`
-	Symbol              string  `json:"symbol"`
-	Latitude            float64 `json:"latitude"`
-	Longitude           float64 `json:"longitude"`
-	Altitude            float64 `json:"altitude"`
-	Speed               float64 `json:"speed"`
-	Temperature         float64 `json:"temperature"`
-	Humidity            float64 `json:"humidity"`
-	RawMessage          string  `json:"raw_message"`
+	Timestamp           int32          `json:"timestamp"`
+	SourceCallsign      string         `json:"src_callsign"`
+	DestinationCallsign string         `json:"dst_callsign"`
+	Status              string         `json:"status"`
+	Symbol              string         `json:"symbol"`
+	Latitude            float64        `json:"latitude"`
+	Longitude           float64        `json:"longitude"`
+	Altitude            float64        `json:"altitude"`
+	Speed               float64        `json:"speed"`
+	Weather             *WeatherReport `json:"weather_report"`
+	RawMessage          string         `json:"raw_message"`
+}
+
+type WeatherReport struct {
+	Temperature    float64 `json:"temp"`
+	Humidity       uint8   `json:"humidity"`
+	InsideHumidity uint8   `json:"humidity_in"`
 }
 
 type AprsParser struct{}
@@ -50,9 +55,18 @@ func (p *AprsParser) parseAprsPacket(message string, isAX25 bool) (*AprsMessage,
 		Timestamp:           int32(time.Now().Unix()),
 		SourceCallsign:      C.GoString(packet.src_callsign),
 		DestinationCallsign: C.GoString(packet.dst_callsign),
-		Latitude:            float64(C.double(*packet.latitude)),
-		Longitude:           float64(C.double(*packet.longitude)),
+		Latitude:            parseNilableFloat(packet.latitude),
+		Longitude:           parseNilableFloat(packet.longitude),
 		RawMessage:          C.GoStringN(packet.body, C.int(packet.body_len)),
+	}
+
+	if packet.wx_report != nil {
+		w := WeatherReport{
+			Temperature:    parseNilableFloat(packet.wx_report.temp),
+			Humidity:       parseNilableUInt(packet.wx_report.humidity),
+			InsideHumidity: parseNilableUInt(packet.wx_report.humidity_in),
+		}
+		parsedMsg.Weather = &w
 	}
 
 	return &parsedMsg, nil
@@ -61,6 +75,22 @@ func (p *AprsParser) parseAprsPacket(message string, isAX25 bool) (*AprsMessage,
 func boolToInt(b bool) int {
 	if b {
 		return 1
+	} else {
+		return 0
+	}
+}
+
+func parseNilableFloat(d *C.double) float64 {
+	if d != nil {
+		return float64(C.double(*d))
+	} else {
+		return 0
+	}
+}
+
+func parseNilableUInt(d *C.uint) uint8 {
+	if d != nil {
+		return uint8(C.uint(*d))
 	} else {
 		return 0
 	}
--- aprs_parser_test.go
+++ aprs_parser_test.go
@@ -33,6 +33,43 @@ func Test_ParseAprsNonAX25PacketWithLocation(t *testing.T) {
 	}
 }
 
+func Test_ParseAprsNonAX25PacketWithLocationAndWeather(t *testing.T) {
+	timeBeforeTest := int32(time.Now().Unix())
+	p := NewParser()
+	defer p.Finish()
+
+	msg, err := p.parseAprsPacket("DW6161>APRS,TCPXX*,qAX,CWOP-4:@101509z5004.48N/00645.00E_049/000g000t046r000p019P013h97b10123WeatherCatV123B16H31", false)
+	timeAfterTest := int32(time.Now().Unix())
+
+	if err != nil {
+		t.Error("Error was unexpectedly non-nil", err)
+	}
+	if msg.Timestamp < timeBeforeTest || msg.Timestamp > timeAfterTest {
+		t.Error("Timestamp was set incorrectly")
+	}
+	if msg.SourceCallsign != "DW6161" {
+		t.Error("Source callsign not parsed correctly", msg.SourceCallsign)
+	}
+	if msg.DestinationCallsign != "APRS" {
+		t.Error("Destination callsign not parsed correctly", msg.DestinationCallsign)
+	}
+	if msg.Latitude != 50.074666666666666 {
+		t.Error("Latitude incorrect", msg.Latitude)
+	}
+	if msg.Longitude != 6.75 {
+		t.Error("Longitude incorrect", msg.Longitude)
+	}
+	if msg.Weather.Temperature != 7.777777777777778 {
+		t.Error("Temperature incorrect", msg.Weather.Temperature)
+	}
+	if msg.Weather.Humidity != 97 {
+		t.Error("Humidity incorrect", msg.Weather.Humidity)
+	}
+	if msg.Weather.InsideHumidity != 0 {
+		t.Error("Inside Humidity incorrect", msg.Weather.InsideHumidity)
+	}
+}
+
 func Test_ParseAprsUnsupportedFormatPacket(t *testing.T) {
 	p := NewParser()
 	defer p.Finish()
