a5f648e55753f8f5e554467143ea417fd5940433
Scott Kidder <kidder.scott@gmail.com>
2015-04-24 08:56:11 -0700

    Added query handler

---
+++ handlers/query_handler.go
@@ -0,0 +1,74 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/go-martini/martini"
+	"github.com/urlgrey/aprs-dashboard/db"
+)
+
+func InitializeRouterForQueryHandlers(m *martini.ClassicMartini) {
+	m.Get("/api/v1/callsign/:callsign", callsignQueryHandler)
+	m.Get("/api/v1/position", positionQueryHandler)
+}
+
+func callsignQueryHandler(req *http.Request, params martini.Params) (int, []byte) {
+	redisHost := strings.TrimLeft(os.Getenv("DB_PORT"), "tcp://")
+	if redisHost == "" {
+		log.Fatal("DB_PORT environment variable is not set, but is required, exiting")
+	}
+	redisPassword := os.Getenv("APRS_REDIS_PASSWORD")
+	redisDatabase := os.Getenv("APRS_REDIS_DATABASE")
+
+	database := db.NewDatabase(redisHost, redisPassword, redisDatabase)
+	defer database.Close()
+
+	page := parseOptionalIntParam(req.URL.Query().Get("page"), 1)
+	records, err := database.GetRecordsForCallsign(params["callsign"], page)
+	if err == nil {
+		body, _ := json.Marshal(records)
+		return http.StatusOK, body
+	} else {
+		log.Println("Unable to find callsign data", params["callsign"])
+		body, _ := json.Marshal("{}")
+		return http.StatusNotFound, body
+	}
+}
+
+func positionQueryHandler(req *http.Request, params martini.Params) (int, []byte) {
+	redisHost := strings.TrimLeft(os.Getenv("DB_PORT"), "tcp://")
+	if redisHost == "" {
+		log.Fatal("DB_PORT environment variable is not set, but is required, exiting")
+	}
+	redisPassword := os.Getenv("APRS_REDIS_PASSWORD")
+	redisDatabase := os.Getenv("APRS_REDIS_DATABASE")
+
+	database := db.NewDatabase(redisHost, redisPassword, redisDatabase)
+	defer database.Close()
+
+	var parseErr error
+	lat, parseErr := parseRequiredFloatParam(req.URL.Query().Get("lat"))
+	long, parseErr := parseRequiredFloatParam(req.URL.Query().Get("long"))
+	time := parseOptionalIntParam(req.URL.Query().Get("time"), 3600)
+	radiusKM := parseOptionalIntParam(req.URL.Query().Get("radius"), 30)
+
+	if parseErr != nil {
+		log.Println("Unable to parse required parameters for lat-long")
+		body, _ := json.Marshal("{}")
+		return http.StatusBadRequest, body
+	} else {
+		records, err := database.GetRecordsNearPosition(lat, long, time, radiusKM)
+
+		if err == nil {
+			body, _ := json.Marshal(records)
+			return http.StatusOK, body
+		} else {
+			body, _ := json.Marshal("{}")
+			return http.StatusInternalServerError, body
+		}
+	}
+}
