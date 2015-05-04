02c0609fab32619f1e498dad6e39518bbfea7e8f
Scott Kidder <kidder.scott@gmail.com>
2015-05-03 21:12:29 -0700

    Added more negroni tests for message handler

--- handlers/message_handler_test.go
+++ handlers/message_handler_test.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
-	"github.com/urlgrey/aprs-dashboard/parser"
+	"github.com/codegangsta/negroni"
 	"github.com/stretchr/testify/assert"
+	"github.com/urlgrey/aprs-dashboard/parser"
 )
 
 func TestHandlerInitialize(t *testing.T) {
@@ -25,4 +30,37 @@ func TestHandlerSubmitAPRSMessage(t *testing.T) {
 	m := MessageHandler{parser: aprsParser}
 	err := m.Initialize()
 	assert.Nil(t, err)
+}
+
+func TestHandleMessageWithMalformedJSON(t *testing.T) {
+	aprsParser := parser.NewParser()
+	aprsParser.Initialize()
+	defer aprsParser.Close()
+	h := MessageHandler{parser: aprsParser}
+	assert.Nil(t, h.Initialize())
+
+	r, _ := http.NewRequest("PUT", "/api/v1/message", strings.NewReader("asdf"))
+	r.Header.Add("Accept", "application/json")
+	rec := httptest.NewRecorder()
+	rw := negroni.NewResponseWriter(rec)
+	rw.Header().Set("Content-Type", "application/json")
+
+	h.SubmitAPRSMessage(rw, r)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+}
+
+func TestHandleMessageWithDisqueUnreachable(t *testing.T) {
+	aprsParser := parser.NewParser()
+	aprsParser.Initialize()
+	defer aprsParser.Close()
+	h := MessageHandler{parser: aprsParser}
+	assert.Nil(t, h.Initialize())
+
+	r, _ := http.NewRequest("PUT", "/api/v1/message", strings.NewReader("asdf"))
+	r.Header.Add("Accept", "application/json")
+	rec := httptest.NewRecorder()
+	rw := negroni.NewResponseWriter(rec)
+
+	h.SubmitAPRSMessage(rw, r)
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
 }
